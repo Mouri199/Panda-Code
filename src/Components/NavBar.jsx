79240a59ba8e38ee7143e7a983e666ed3499a370
Mouri199 <138799900+Mouri199@users.noreply.github.com>
2024-01-13 13:23:59 +0600

    all done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,10 @@ import { FiMenu } from "react-icons/fi";
 
 const NavBar = () => {
     return (
-       
-        
-       
-        <div className='overflow-hidden'>
+
+
+
+        <div>
             <div className="drawer ">
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
@@ -33,23 +33,21 @@ const NavBar = () => {
                                 <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
                                 <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
                                     {/* Sidebar content here */}
-                                    <NavLink className='text-xl hover:text-[#0062FF] hover:underline' to='/'>Home</NavLink>
-                                    <NavLink className='text-xl hover:text-[#0062FF] hover:underline' to='/internship'>Internship</NavLink>
-                                    <details className="dropdown">
-                                        <summary className="text-xl">Tutorial</summary>
-                                        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                                            <Link to='/html'><a>C++</a></Link>
-                                            <Link to='/html'><a>Python</a></Link>
-                                            <Link to='/html'><a>JAVA</a></Link>
-                                            <Link to='/html'><a>HTML</a></Link>
-                                            <Link to='/html'><a>CSS</a></Link>
-                                            <Link to='/html'><a>JAVASCRIPT</a></Link>
-                                            <Link to='/html'><a>PHP</a></Link>
-                                            <Link to='/html'><a>MongoDB</a></Link>
-                                        </ul>
-                                    </details>
+                                    <div>
+                                        <img className='h-[70px]' src={logo} alt="" />
+                                    </div>
+                                    <div className='flex flex-col p-10 pt-[200px] gap-5'>
+                                        <a className='text-xl hover:text-[#0062FF] hover:underline' to='/'>Facebook</a>
+                                        <a className='text-xl hover:text-[#0062FF] hover:underline' to='/'>Twitter</a>
+                                        <a className='text-xl hover:text-[#0062FF] hover:underline' to='/'>Dribble</a>
+                                        <a className='text-xl hover:text-[#0062FF] hover:underline pb-72' to='/'>Instagram</a>
+                                    </div>
+                                    <hr />
+                                    <div>
+                                        <h1 className='text-2xl font-bold'> +1 840 9841 25 69</h1>
+                                        <p className='pt-2'>info@gmail.com</p>
+                                    </div>
 
-                                    <NavLink className='text-xl hover:text-[#0062FF] hover:underline' to='/courses'>Courses</NavLink>
                                 </ul>
                             </div>
                         </div>
@@ -60,34 +58,79 @@ const NavBar = () => {
                         <div className="flex-none hidden lg:block">
                             <ul className="menu gap-5 font-medium menu-horizontal">
                                 {/* Navbar menu content here */}
-                                <div className="dropdown dropdown-hover">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Home</div>
-                                    <ul tabIndex={0} className="dropdown-content z-[50] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><a>Item 1</a></li>
-                                        <li><a>Item 2</a></li>
-                                    </ul>
-                                </div>
-                                <div className="dropdown dropdown-hover">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Pages</div>
-                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><a>Item 1</a></li>
-                                        <li><a>Item 2</a></li>
-                                    </ul>
-                                </div>
-                                <div className="dropdown dropdown-hover">
-                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Protfolio</div>
-                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><a>Item 1</a></li>
-                                        <li><a>Item 2</a></li>
+                                <div className="dropdown dropdown-hover ">
+                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1 ">Home</div>
+                                    <ul tabIndex={0} className=" dropdown-content  menu p-2 shadow bg-[#0B0C22] text-white rounded-box w-52">
+                                        <li><a>Marketing Agency</a></li>
+                                        <li><a>Creative Agency</a></li>
+                                        <li><a>Advertising Agency</a></li>
                                     </ul>
                                 </div>
                                 <div className="dropdown dropdown-hover">
                                     <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Blog</div>
-                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                                        <li><a>Item 1</a></li>
-                                        <li><a>Item 2</a></li>
+
+                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#0B0C22] text-white rounded-box w-[600px]">
+                                        <div className='flex gap-5'>
+                                            <div>
+                                                <li className='text-2xl font-semibold'><a>Blog Style 1</a></li>
+                                                <li><a>Blog Standard</a></li>
+                                                <li><a>Blog List</a></li>
+                                                <li><a>Masonry 2 Columns</a></li>
+                                                <li><a>Masonry 3 Columns</a></li>
+                                                <li><a>Masonry 4 Columns</a></li>
+                                            </div>
+                                            <div>
+                                                <li className='text-2xl font-semibold'><a>Blog Style 2</a></li>
+                                                <li><a>Blog Grid 3 Columns</a></li>
+                                                <li><a>Blog Grid 4 Columns</a></li>
+                                                <li><a>Blog Protfolio 3 Columns</a></li>
+                                                <li><a>Blog Protfolio 4 Columns</a></li>
+
+                                            </div>
+                                            <div>
+                                                <li className='text-2xl font-semibold'><a>Single Post</a></li>
+                                                <li><a>Style 1</a></li>
+                                                <li><a>Style 2</a></li>
+                                                <li><a>Style 3</a></li>
+                                                <li><a>Video Post</a></li>
+                                                <li><a>Audio Post</a></li>
+                                                <li><a>With Sidebar</a></li>
+
+                                            </div>
+                                        </div>
+                                    </ul>
+
+                                </div>
+                                <div className="dropdown dropdown-hover ">
+                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Pages</div>
+                                    <ul tabIndex={0} className=" dropdown-content  menu p-2 shadow bg-[#0B0C22] text-white rounded-box w-52">
+                                        <li><a>About</a></li>
+                                        <li><a>Services</a></li>
+                                        <li><a>Our Team</a></li>
+                                        <li><a>FAQ</a></li>
+                                        <li><a>Pricing</a></li>
+                                        <li><a>Typography</a></li>
+                                        <li><a>Shortcodes</a></li>
+                                        <li><a>404</a></li>
+                                        <li><a>Privacy Policy</a></li>
+                                        <li><a>Service Plus</a></li>
+                                        <li><a>Newsletter Popup</a></li>
                                     </ul>
                                 </div>
+
+                                <div className="dropdown dropdown-hover">
+                                    <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Protfolio</div>
+                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#0B0C22] text-white rounded-box w-52">
+                                        <li><a>Protfolio 1</a></li>
+                                        <li><a>Protfolio 2</a></li>
+                                        <li><a>Protfolio 3</a></li>
+                                        <li><a>Protfolio 4</a></li>
+                                        <li><a>Protfolio 5</a></li>
+                                        <li><a>Protfolio 6</a></li>
+                                        <li><a>SingleProtfolio</a></li>
+                                    </ul>
+                                </div>
+
                                 <div className="dropdown dropdown-hover">
                                     <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">Contacts</div>
 
@@ -100,9 +143,16 @@ const NavBar = () => {
 
                         </div>
                         <div>
-                            <button className="btn btn-ghost btn-circle">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                            </button>
+
+
+                            <div className="dropdown dropdown-top dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost hover:bg-[#F9FAFF] text-lg hover:underline m-1">
+                                    {/* <button className="btn btn-ghost btn-circle"> */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                                </div>
+
+                            </div>
+
                         </div>
                         <button className='px-10 btn py-2 bg-[#FF2E57] hover:bg-[#FF2E57] text-xl font-bold rounded-full text-white flex items-center'>
                             Let's Talk
